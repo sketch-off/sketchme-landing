5585635b5b652716f38b09d41c2e4fc36ef8276d
Frank Luan <franklsf95@gmail.com>
2015-02-12 11:22:45 -0800

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@
 echo "$(tput setaf 2)Building Jekyll... $(tput sgr0)"
 jekyll build
 echo "$(tput setaf 2)Syncing with AWS S3... $(tput sgr0)"
-aws s3 sync _site s3://sketchme.co/ --region us-west-2
+BUCKET=sketchme.co
+aws s3 rm s3://$BUCKET/assets --recursive
+aws s3 sync _site s3://$BUCKET/ --region us-west-2 --exclude .DS_Store
